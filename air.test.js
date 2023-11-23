c3cec0a6e9aecab7ad2b45db6cd705e0a337bb01
Alexander Sulim <hello@sul.im>
2023-11-23 22:26:55 +0100

    Rewrite self-tests for Runner
    Use Runner to run self-tests.

--- air.test.js
+++ air.test.js
@@ -1,29 +1,37 @@
 import { Runner } from "./air.js"
 
-// A dummy test suite.
-class Suite {
+// A test suite to run a self-check on Runner.
+class SelfCheckSuite {
   constructor() {
     this.runned = new Array();
   }
   
-  get test_getter_func() { throw new Error("access to a getter detected"); }
+  get test_getter_func() {
+    this.runned.push("test_getter_func");
+  }
 
   test_sync_func() {
     this.runned.push("test_sync_func");
   }
 }
 
-Deno.test("Runner_run", () => {
-  let runner = new Runner();
-  let suite = new Suite();
-  
-  runner.run(suite);
+let suite = new SelfCheckSuite();
 
-  if (suite.runned.length !== 1) {
-    throw new Error(`want length of [${suite.runned.join(",")}] to be 1, got ${suite.runned.length}`);
-  }
-  
-  if (!suite.runned.includes("test_sync_func")) {
-    throw new Error(`want "test_sync_func" to be included in [${suite.runned.join(",")}]`);
-  }
-});
+let runner = new Runner();
+runner.run(suite);
+
+// Self-checks
+
+if (suite.runned.length !== 1) {
+  throw new Error(`want length of [${suite.runned.join(",")}] to be 1, got ${suite.runned.length}`);
+}
+
+// Functions with names starting with "test_" prefix must be treated as tests.
+if (!suite.runned.includes("test_sync_func")) {
+  throw new Error(`want "test_sync_func" to be included in [${suite.runned.join(",")}]`);
+}
+
+// Getters even with names starting with "test_" prefix must be ignored.
+if (suite.runned.includes("test_getter_func")) {
+  throw new Error(`want "test_getter_func" not to be included in [${suite.runned.join(",")}]`);
+}
