8a2527b00be093c4e77a6963eb2fc8336db068b7
Alexander Sulim <hello@sul.im>
2023-11-24 08:54:23 +0100

    Update self-test suite

--- air.js
+++ air.js
@@ -1,24 +1,29 @@
 // Runner executes test suites.
 //
-// A test suite is a class. Any method which name starts with the "test_"
+// A test suite is a class. Any method with its name starting with the "test_"
 // prefix is treated as a test.
 class Runner {
+  // IDEA: Make the prefix configurable.
+  get #prefix() {
+    return "test_";
+  }
+
   run(suite) {
     let prototype = Object.getPrototypeOf(suite);
     let properties = Object.getOwnPropertyNames(prototype);
 
     // TODO: Randomize tests.
 
-    properties.forEach(function (property) {
-      // TODO: Make the prefix configurable.
-      // TODO: Change the prefix to be "test" (no underscore).
-      if (!property.startsWith("test_")) return;
+    properties.forEach((property) => {
+      if (!property.startsWith(this.#prefix)) return;
 
       let descriptor = Object.getOwnPropertyDescriptor(prototype, property);
 
       if (typeof (descriptor.value) !== "function") return;
+
       // TODO: Add support for async functions.
 
+      // IDEA: Add support for fail-fast functionality.
       try {
         descriptor.value.call(suite);
       } catch (err) {
--- air.test.js
+++ air.test.js
@@ -1,37 +1,52 @@
-import { Runner } from "./air.js"
+import { Runner } from "./air.js";
 
 // A test suite to run a self-check on Runner.
-class SelfCheckSuite {
+class Suite {
   constructor() {
     this.runned = new Array();
-  }
-  
-  get test_getter_func() {
-    this.runned.push("test_getter_func");
   }
 
   test_sync_func() {
     this.runned.push("test_sync_func");
   }
+
+  get test_getter_func() {
+    throw new Error("detected access to a getter method: 'test_getter_func'");
+  }
+
+  testing() {
+    throw new Error("detected access to a non-test method: 'testing'");
+  }
+
+  TestSyncFunc() {
+    throw new Error("detected access to a non-test method: 'TestSyncFunc'");
+  }
+
+  testSyncFunc() {
+    throw new Error("detected access to a non-test method: 'testSyncFunc'");
+  }
 }
 
-let suite = new SelfCheckSuite();
+let suite = new Suite();
 
 let runner = new Runner();
 runner.run(suite);
 
-// Self-checks
+// Methods with names starting with the "test_" prefix must be treated as tests.
+let exp = ["test_sync_func"];
 
-if (suite.runned.length !== 1) {
-  throw new Error(`want length of [${suite.runned.join(",")}] to be 1, got ${suite.runned.length}`);
+if (suite.runned.length !== exp.length) {
+  throw new Error(
+    `want length of [${
+      suite.runned.join(", ")
+    }] to be ${exp.length}, got ${suite.runned.length}`,
+  );
 }
 
-// Functions with names starting with "test_" prefix must be treated as tests.
-if (!suite.runned.includes("test_sync_func")) {
-  throw new Error(`want "test_sync_func" to be included in [${suite.runned.join(",")}]`);
-}
-
-// Getters even with names starting with "test_" prefix must be ignored.
-if (suite.runned.includes("test_getter_func")) {
-  throw new Error(`want "test_getter_func" not to be included in [${suite.runned.join(",")}]`);
-}
+exp.forEach((test) => {
+  if (!suite.runned.includes(test)) {
+    throw new Error(
+      `want '${test}' to be included in [${suite.runned.join(", ")}]`,
+    );
+  }
+});
